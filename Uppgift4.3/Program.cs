054983b8cd9e1e6be7565b1f60e53903ba74521c
arun06001 <arun06001@E-PW0234KE>
2023-10-18 12:46:53 +0200

    Add project files.

---
+++ Uppgift4.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Uppgift4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string svar = "";
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0) //Lägger till fizz och buzz om det är delbart med 3 resp. 5
+                {
+                    svar = "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    svar += "Buzz";
+                }
+
+                if (svar == "") // om det inte är delbart är svar "" så i skrivs istället
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(svar);
+                }
+
+                svar = "";
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
